eff4ab79d6e5eb5fb1cc0fcf3f36f6b1186658bc
Calvi <calvi.alejo@gmail.com>
2022-09-22 00:02:56 -0300

    feat: mail

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,53 @@
 import React from 'react'
 
-import { Button, Container, FormLabel, FormControl, Input, Stack, Text } from '@chakra-ui/react'
+import { Button, Container,FormLabel, FormControl, Input, Stack, Text } from '@chakra-ui/react'
 
 import imagen1 from './assets/img1.png'
 
 function App () {
   return (
-    <Container  maxWidth={'10xl'} maxHeight={'2xl'} justifyItems={'center'} alignItems={'center'} padding={10} backgroundImage={imagen1} bgSize={1380}>
+    <Container  maxWidth={'10xl'} maxHeight={'2xl'} justifyItems={'center'} alignItems={'center'} padding={10} backgroundImage={imagen1} bgSize={1440} bgRepeat={'none'}>
 
       <Stack alignItems={'center'} justifyContent={'center'} spacing={8}   >
         {/* <Stack padding={6} bg= {'skyblue'}>
           <Text textAlign={'center'} fontSize= {{ base: 'xl', sm: '2xl', md: '3xl' }}>Seguimiento Satelital</Text>
         </Stack> */}
 
-        <Stack  padding={10} alignItems={'center'} justifyContent={'center'} fontSize= {{ base: 'xl', sm: '2xl', md: '3xl' }}
+        <Stack  maxW={650} maxH={620} padding={10} alignItems={'center'} justifyContent={'center'} fontSize= {{ base: 'xl', sm: '2xl', md: '3xl' }}
               
               bg= {'blackAlpha.600' }
-              spacing={6} borderRadius={50}>
+              spacing={4} borderRadius={50}>
 
 
             <Stack>
-                <Text color={'white'} fontWeight={'bold'} textAlign={'center'} >Solicita una cotización ahora!</Text>
+                <Text color={'white'} fontWeight={'bold'} textAlign={'center'} >¡Solicitá una cotización ahora!</Text>
             </Stack>
 
             
 
             <FormControl >
 
-              <Stack direction={'row'} >
+              <Stack direction={['column', 'row']} >
                 
                 <Stack >
                       <FormLabel fontWeight={'bold'} color={'white'}>Nombre *</FormLabel>
-                      <Input  placeholder='Nombre' type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }} type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>DNI *</FormLabel>
-                      <Input placeholder='N° Documento' type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }} type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>Localidad *</FormLabel>  
-                      <Input type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>Telefono Celular *</FormLabel>
-                      <Input type="text" /> 
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" /> 
               </Stack>
               <Stack >
                       <FormLabel fontWeight={'bold'} color={'white'}>Apellido *</FormLabel>
-                      <Input placeholder='Apellido' type="text" color={'red.800'} />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>Domicilio *</FormLabel>
-                      <Input placeholder='Calle' type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>Codigo Postal *</FormLabel>  
-                      <Input type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" />
                       <FormLabel fontWeight={'bold'} color={'white'}>Telefono Alternativo</FormLabel>
-                      <Input type="text" />
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }}  type="text" />
               </Stack>
 
                 
@@ -55,15 +55,15 @@ function App () {
               </Stack>
               
         
-              {/* <Stack >
-                      <FormLabel fontWeight={'bold'}>Email</FormLabel>
-                      <Input placeholder='Email' type="email" />
-                      <FormLabel fontWeight={'bold'}>Email Alternativo</FormLabel>
-                      <Input placeholder='Email' type='email' />
-              </Stack> */}
+              <Stack >
+                      <FormLabel fontWeight={'bold'} color={'white'}>Email *</FormLabel>
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }} type="email" />
+                      <FormLabel fontWeight={'bold'} color={'white'}>Email Alternativo</FormLabel>
+                      <Input color={'white'} placeholder='...' _placeholder={{ opacity: 1, color: 'white' }} type='email' />
+              </Stack>
               
-              <Stack mt={6} alignItems={'center'}>
-              <Button type='submit'>Submit</Button>
+              <Stack mt={4} alignItems={'center'}>
+              <Button type='submit'>Enviar</Button>
               </Stack>
               
                            
